4b811f531b5b243e2a39006ea95ee8b835bc838e
Arpit Kumar <arpitk2806@gmail.com>
2024-05-22 14:32:15 +0530

    Added Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
     }
 }
 
@@ -126,3 +129,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
+
+#Custom Admin Fields
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" style="width: 50px; border-radius: 50px;"/>'.format(object.photo.url))
+    thumbnail.short_description = 'Photo'
+
+    list_display = ('id','thumbnail','first_name', 'last_name', 'designation', 'created_date')
+    list_display_links = ('id','thumbnail','first_name',)
+    search_fields = ('first_name', 'last_name', 'designation')
+    list_filter = ('designation',)
 
 # Register your models here.
+admin.site.register(Team, TeamAdmin)
