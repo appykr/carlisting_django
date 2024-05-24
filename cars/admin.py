eda6963618c886bbc8241e54bc0d05fd194f3127
Arpit Kumar <arpitk2806@gmail.com>
2024-05-24 17:14:55 +0530

    Modified Admin Panel

--- cars/admin.py
+++ cars/admin.py
@@ -8,7 +8,7 @@ class CarAdmin(admin.ModelAdmin):
         return format_html('<img src="{}" style="width: 50px; border-radius: 50px;"/>'.format(object.car_photo.url))
     thumbnail.short_description = 'Photo'
 
-    list_display = ('id','thumbnail','car_title','year', 'created_date','is_featured')
+    list_display = ('id','thumbnail','car_title','year', 'price','is_featured','state')
     list_display_links = ('id','thumbnail','car_title')
     search_fields = ('car_title','year', 'model')
     list_editable = ('is_featured',)
--- cars/views.py
+++ cars/views.py
@@ -9,8 +9,16 @@ def cars(requests):
     paginator = Paginator(all_cars, 4)
     page = requests.GET.get('page')
     paged_cars = paginator.get_page(page)
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    state_search = Car.objects.values_list('state', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct().order_by('year')
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
     context = {
         'all_cars': paged_cars,
+        'model_search': model_search,
+        'state_search' : state_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
     return render(requests, 'cars/cars.html', context)
 
@@ -23,11 +31,50 @@ def car_detail(requests, id):
 
 def search(requests):
     cars = Car.objects.order_by('-created_date')
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    state_search = Car.objects.values_list('state', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct().order_by('year')
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission', flat=True).distinct()
+
+
     if 'keyword' in requests.GET:
         keyword = requests.GET['keyword']
         if keyword:
             cars = cars.filter(description__icontains=keyword)
+
+    if 'model' in requests.GET:
+        model = requests.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+    
+    if 'state' in requests.GET:
+        state = requests.GET['state']
+        if state:
+            cars = cars.filter(state__iexact=state)
+
+    if 'year' in requests.GET:
+        year = requests.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_style' in requests.GET:
+        body_style = requests.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if 'min_price' in requests.GET:
+        min_price = requests.GET['min_price']
+        max_price = requests.GET['max_price']
+        if max_price:
+           cars = cars.filter(price__gte=min_price, price__lte=max_price)
+    
     context = {
         'cars': cars,
+        'model_search': model_search,
+        'state_search' : state_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
     }
     return render(requests, 'cars/search.html', context)
